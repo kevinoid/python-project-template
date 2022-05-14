kevinoid/python-project-template

commit de388c2ee51ec8197e78c7fa5b50ff27759464b5

Use the MIT License for projects
Since GNU and OSI recommend against CC0 for software (due to the
explicit patent disclaimer) and the MIT license is the most popular on
PyPI and it matches my intent for my most common projects, use it
instead.

Keep the template CC0 to make it easy for anyone to use for any purpose
without the need to keep any of its licensing information in software
based on the template.

Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

src/packagename/cli.py
@@ -1,5 +1,5 @@
 # This file is part of python-project-template
-# Made available under CC0 1.0 Universal, see LICENSE.txt
+# Made available under the terms of the MIT License, see LICENSE.txt
 # Copyright 2019-2020 Kevin Locke <kevin@kevinlocke.name>
 """Command-line interface for packagename."""
 
@@ -19,13 +19,12 @@ _VERSION_MESSAGE = (
 Copyright 2019-2020 Kevin Locke <kevin@kevinlocke.name>
 
 %(prog)s is free software; you can redistribute it and/or modify
-it under the terms of the CC0 1.0 Universal (CC0 1.0) Public Domain
-Dedication published by Creative Commons.
+it under the terms of the MIT License.
 
 %(prog)s is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-CC0 1.0 Universal (CC0 1.0) Public Domain Dedication for more details.'''
+the terms of the MIT License for more details.'''
 )
 
 _logger = logging.getLogger(__name__)

setup.cfg
@@ -11,7 +11,7 @@ long_description = file: README.rst
 # Note: license is only needed for deviations/exceptions to License classifier
 # https://packaging.python.org/guides/distributing-packages-using-setuptools/?highlight=keywords#license
 # Is also printed by `./setup.py --license`.  Match classifier where possible.
-license = CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+license = MIT License
 license_file = LICENSE.txt
 author = Kevin Locke
 author_email = kevin@kevinlocke.name
@@ -27,7 +27,7 @@ project_urls =
 classifiers =
     Development Status :: 3 - Alpha
     Intended Audience :: Developers
-    License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+    License :: OSI Approved :: MIT License
     Operating System :: OS Independent
     Programming Language :: Python
     Programming Language :: Python :: 3

setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # This file is part of packagename <https://github.com/kevinoid/packagename>
-# Made available under CC0 1.0 Universal, see LICENSE.txt
+# Made available under the terms of the MIT License, see LICENSE.txt
 # Copyright 2019-2020 Kevin Locke <kevin@kevinlocke.name>
 """
 Distutils/Setuptools Setup Script.

src/packagename/__init__.py
@@ -1,5 +1,5 @@
 # This file is part of python-project-template
-# Made available under CC0 1.0 Universal, see LICENSE.txt
+# Made available under the terms of the MIT License, see LICENSE.txt
 # Copyright 2019-2020 Kevin Locke <kevin@kevinlocke.name>
 """packagename module."""
 

src/packagename/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # This file is part of python-project-template
-# Made available under CC0 1.0 Universal, see LICENSE.txt
+# Made available under the terms of the MIT License, see LICENSE.txt
 # Copyright 2019-2020 Kevin Locke <kevin@kevinlocke.name>
 """Entry point for running packagename as a command-line tool."""
 

commit ce8f5321051a66da8cd14852551b12afeda53daa

fix comment mentioning CC0
This was missed when switching from CC0 to MIT.

Fixes: de388c2ee51e ("Use the MIT License for projects")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

src/packagename/cli.py
@@ -11,7 +11,7 @@ from typing import Optional
 from . import __version__
 
 # Short license message varies by license.
-# The text below is based on the GPL short text, modified for CC0.
+# The text below is based on the GPL short text, modified for the MIT License.
 # Change as appropriate.
 _VERSION_MESSAGE = (
     '%(prog)s '